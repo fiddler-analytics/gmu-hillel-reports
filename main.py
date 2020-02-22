dfeeb69de95e58a592af663abd38baeb62166baf
matt <mthw.wm.robinson@gmail.com>
2020-02-22 16:37:00 -0500

    implemented function to write the report to csv

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """Pulls contact info from HEART and checks to see if that contact
 info exists in Little Green Light. Produces a report that lists
 any e-mail address or phone numbers that appear in HEART but not in LGL."""
+import csv
+from collections import OrderedDict
+import datetime
 import json
 import time
 import os
@@ -14,16 +17,94 @@ LGL = LittleGreenLight()
 
 # The keys are the field names that appear in HEART and the entries
 # are the column names in the final csv
-HEART_CONTACT_FIELDS = {
-    'Id': 'id',
-    'FirstName': 'First Name',
-    'LastName': 'Last Name',
-    'Email': 'Email',
-    'npe01__AlternateEmail__c': 'Alternate Email',
-    'npe01__HomeEmail__c': 'Home Email',
-    'MobilePhone': 'Mobile Phone',
-    'HomePhone': 'Home Phone'
-}
+HEART_CONTACT_FIELDS = OrderedDict([
+    ('FirstName', 'First Name'),
+    ('LastName', 'Last Name'),
+    ('Email', 'Email'),
+    ('npe01__AlternateEmail__c', 'Alternate Email'),
+    ('npe01__HomeEmail__c', 'Home Email'),
+    ('MobilePhone', 'Mobile Phone'),
+    ('HomePhone', 'Home Phone')
+])
+
+
+def build_missing_contact_report():
+    """Builds a .csv file that shows any contact in the HEART database
+    who either (1) does not appear in LGL or (2) has at least one piece
+    of contact information that does not appear in LGL. Creates a file
+    with the naming convention missing_contact_report_<date>.csv"""
+    heart_contacts = get_heart_contact_info()
+    names, emails, phones = get_lgl_contact_info()
+
+    report_rows = _build_report_rows(heart_contacts, names, emails, phones)
+    _save_missing_contact_report(report_rows)
+
+
+def _save_missing_contact_report(report_rows):
+    """Saves the missing contact report as a csv file
+
+    Parameters
+    ----------
+    report_rows : list
+        a list of dictionaries, which correspond go the rows in the report
+    """
+    csv_columns = [HEART_CONTACT_FIELDS[k] for k in HEART_CONTACT_FIELDS]
+    csv_columns.append('Missing')
+    today = datetime.datetime.today()
+    filename = 'missing_contact_report_{}.csv'.format(today.date())
+
+    try:
+        with open(filename, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+            writer.writeheader()
+            for row in report_rows:
+                writer.writerow(row)
+        print('Wrote the report to {}'.format(filename))
+    except IOError:
+        print('Failed to write the report to CSV.')
+
+
+def _build_report_rows(heart_contacts, names, emails, phones):
+    """Uses the results of the HEART and LGL queries to build the rows
+    for the missing contact report
+
+    Parameters
+    ----------
+    heart_contact : list
+        the list of contacts in HEART CRM
+    names : set
+        the set of names from LGL
+    emails : set
+        the set of emails from LGL
+    phones : set
+        the set of phone numbers from LGL
+
+    Returns
+    -------
+    report_rows : list
+        a list of dictionaries, which correspond go the rows in the report
+    """
+    report_rows = list()
+    for contact in heart_contacts:
+        missing = set()
+        for field in contact:
+            if (contact['FirstName'], contact['LastName']) not in names:
+                missing.add('Name')
+            if 'email' in field.lower() and contact[field] not in emails:
+                missing.add('Email')
+            if 'phone' in field.lower() and contact[field] not in phones:
+                missing.add('Phone')
+
+        # Only write rows to the report if they have missing contact info.
+        if any(missing):
+            row = dict()
+            for field in HEART_CONTACT_FIELDS:
+                clean_field = HEART_CONTACT_FIELDS[field]
+                row[clean_field] = contact[field]
+            row['Missing'] = ', '.join(missing)
+            report_rows.append(row)
+
+    return report_rows
 
 
 def get_lgl_contact_info():
@@ -114,14 +195,14 @@ def get_heart_contact_info():
 
     Returns
     -------
-    results : collections.OrderedDict
-        the resutls of the SOQL query against Salesforce
+    results : list
+        the records of the SOQL query against Salesforce
     """
     heart = _initialize_heart()
     fields = ', '.join(HEART_CONTACT_FIELDS)
     soql = "SELECT {} FROM Contact".format(fields)
     results = heart.query_all(soql)
-    return results
+    return results['records']
 
 
 def _initialize_heart():
@@ -145,3 +226,7 @@ def _initialize_heart():
     return HeartCRM(client_secret=client_secret, client_id=client_id,
                     redirect_uri=redirect_uri, access_code=access_code,
                     sandbox=sandbox)
+
+
+if __name__ == '__main__':
+    build_missing_contact_report()
